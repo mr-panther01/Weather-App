727c9731822e3fcec8354c08eca433584de3efb0
Aditya Sharma <adityasharma92006@gmail.com>
2025-02-06 14:09:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const temperatureField = document.querySelector('.temp');
+const conditionField = document.querySelector('.weather_condition span');
+const timeandDateField = document.querySelector('.time_location span');
+const locationField = document.querySelector('.time_location p');
+const weatherIcon = document.querySelector('.weather_condition img');
+
+const form = document.querySelector('form');
+const searchField = document.querySelector('.searchField');
+
+let target = 'Mumbai';
+
+form.addEventListener('submit', searchFn);
+
+function searchFn(event) {
+    event.preventDefault();
+    let target = searchField.value;
+    fetchData(target);
+}
+
+async function fetchData(target) {
+    let endpoint = `http://api.weatherapi.com/v1/current.json?key=f3c4cf44c66243b39ee51501250602&q=${target}&aqi=no`;
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    console.log(data);
+    let cityName = data.location.name;
+    let Time = data.location.localtime;
+    let Tempc = data.current.temp_c;
+    let condition = data.current.condition.text;
+    let conditionIcon = data.current.condition.icon;
+
+    console.log(Time);
+    console.log(cityName);
+    console.log(Tempc);
+    console.log(condition);
+    console.log(conditionIcon);
+
+    temperatureField.textContent = `${Tempc}°C`;
+    conditionField.textContent = condition;
+    timeandDateField.textContent = Time;
+    locationField.textContent = cityName;
+    weatherIcon.src = conditionIcon;
+}
